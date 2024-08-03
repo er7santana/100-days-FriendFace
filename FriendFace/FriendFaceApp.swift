86f5077795977391b4f3a469e7ec63cdd4b29029
Eliezer Sant Ana <eliezer.ana@f1rst.com.br>
2024-08-03 11:55:00 -0300

    Initial Commit

---
+++ FriendFace/FriendFaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendFaceApp.swift
+//  FriendFace
+//
+//  Created by Eliezer Rodrigo Beltramin de Sant Ana on 03/08/24.
+//
+
+import SwiftUI
+
+@main
+struct FriendFaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
